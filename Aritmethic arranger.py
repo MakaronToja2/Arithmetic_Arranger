9bb2cc094a4122635de5e257f96c78800abda55e
Jakub Wloch <89346176+MakaronToja2@users.noreply.github.com>
2021-09-07 18:59:54 +0200

    Add files via upload

---
+++ Aritmethic arranger.py
@@ -0,0 +1,57 @@
+import re
+
+
+def arithmetic_arranger(problems, statprint=False):
+  first = ''
+  second = ''
+  sumx = ''
+  lines = ''
+  string = ''
+
+  if (len(problems) > 5):
+    return "Error: Too many problems"
+
+  # split to get numbers and - or +
+  for problem in problems:
+    if (re.search("[^\s0-9.+-]", problem)):
+      return ("Error: Numbers must only contain digits")
+    elif (re.search("[/]", problem) or re.search("[*]", problem)):
+      return ("Error: operator must be + or -")
+
+    # split it into numbers and operator
+
+    firstnum = problem.split(" ")[0]
+    secondnum = problem.split(" ")[2]
+    operator = problem.split(" ")[1]
+
+    sum = ""
+    if (operator == "+"):
+      sum = str(int(firstnum) + int(secondnum))
+    elif (operator == "-"):
+      sum = str(int(firstnum) - int(secondnum))
+
+    # Set length of sum and top and bottom and line values
+    length = max(len(firstnum), len(secondnum)) + 2
+    top = str(firstnum).rjust(length)
+    bottom = operator + str(secondnum).rjust(length - 1)
+    line = ''
+    res = str(sum).rjust(length)
+    for s in range(length):
+      line += '-'
+
+    if problem != problems[-1]:
+      first += top + '    '
+      second += bottom + '    '
+      lines += line + '    '
+      sumx += res + '    '
+    else:
+      first += top
+      second += bottom
+      lines += line
+      sumx += res
+
+  if statprint:
+    string = first + '\n' + second + '\n' + lines + '\n' + sumx
+  else:
+    string = first + '\n' + second + '\n' + lines
+  return string
